51d85f34f7d1954e0ffdf967775c5921bdc6dac2
AgusPaez <agus.123.paez@gmail.com>
2024-06-25 00:44:25 -0300

    definitive products

--- src/main/java/orders/repositories/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package orders.repositories;
-
-import orders.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
